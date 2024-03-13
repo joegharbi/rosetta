d5c5e615f12150a3b86a3329f2aa797740dbc91d
Youssef Gharbi <joe.gharbi@gmail.com>
2024-03-13 17:20:37 +0100

    Added new rosetta problems.

---
+++ general_fizzbuzz.erl
@@ -0,0 +1,23 @@
+-module(general_fizzbuzz).
+-export([run/2]).
+-spec run(N :: pos_integer(), Factors :: list(tuple())) -> ok.
+
+fizzbuzz(N, [], []) ->
+    integer_to_list(N);
+fizzbuzz(_, [], Result) ->
+    lists:flatten(lists:reverse(Result));
+fizzbuzz(N, Factors, Result) ->
+    [{Factor, Output}|FactorsRest] = Factors,
+
+    NextResult = case N rem Factor of
+        0 -> [Output|Result];
+        _ -> Result
+    end,
+    
+    fizzbuzz(N, FactorsRest, NextResult).
+
+run(N, Factors) ->
+    lists:foreach(
+        fun(S) -> io:format("~s~n", [S]) end,
+        [fizzbuzz(X, Factors, []) || X <- lists:seq(1, N)]
+    ).
